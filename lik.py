8eb56e0766062e1196c489242a3fae0791d76ef2
Aaron James Stern <ajstern@ln003.brc>
2020-06-03 12:32:38 -0700

    timeb parsing

--- lik.py
+++ lik.py
@@ -25,7 +25,7 @@ def parse_clues(filename):
                 except OSError:
                     print('Error: Unable to open ' + filename)
                     exit(1)
-
+           
         #get #mutations and #sampled trees per mutation
         filepos = 0
         num_muts, num_sampled_trees_per_mut = np.frombuffer(data[slice(filepos, filepos+8, 1)], dtype = np.int32)
@@ -34,21 +34,23 @@ def parse_clues(filename):
         filepos += 8
         #iterate over mutations
         for m in range(0,num_muts):
-            if m > 0:
-               print("Warning: multiple mutations.")
-            bp, daf, n = np.frombuffer(data[slice(filepos, filepos+12, 1)], dtype = np.int32)
-            filepos   += 12
-            #print("BP: %d, DAF: %d, n: %d" % (bp, daf, n))
-
+            bp = np.frombuffer(data[slice(filepos, filepos+4, 1)], dtype = np.int32)
+            filepos += 4
+            anc, der = np.frombuffer(data[slice(filepos, filepos+2, 1)], dtype = 'c')
+            filepos += 2
+            daf, n = np.frombuffer(data[slice(filepos, filepos+8, 1)], dtype = np.int32)
+            filepos += 8
+            #print("BP: %d, anc: %s, der %s, DAF: %d, n: %d" % (bp, str(anc), str(der), daf, n))
+            
             num_anctimes = 4*(n-daf-1)*num_sampled_trees_per_mut
             anctimes     = np.reshape(np.frombuffer(data[slice(filepos, filepos+num_anctimes, 1)], dtype = np.float32), (num_sampled_trees_per_mut, n-daf-1))
             filepos     += num_anctimes
+            #print(anctimes)
             
-
             num_dertimes = 4*(daf-1)*num_sampled_trees_per_mut
             dertimes     = np.reshape(np.frombuffer(data[slice(filepos, filepos+num_dertimes, 1)], dtype = np.float32), (num_sampled_trees_per_mut, daf-1))
             filepos     += num_dertimes
-          
+         
     return dertimes,anctimes 
 
 def _args(super_parser,main=False):
